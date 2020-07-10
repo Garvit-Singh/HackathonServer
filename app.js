2015c3b8f07c09a54b1c8ab48d6cb25dec3501fd
Garvit-Singh <65304346+Garvit-Singh@users.noreply.github.com>
2020-07-10 11:57:35 +0530

    cors and body parser

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(cors())
 
 app.use((req,res,next) => {
-  res.status(201).send('Welcome to Server')
+  res.status(201).send('Welcome to Server ')
+})
+
+app.use((req,res,next) => {
+  const error = new Error('NOT FOUND')
+  error.status = 404
+  next(error)
+})
+
+app.use((error,req,res,next) => {
+  res.status( error.status || 500) 
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
 })
 
 module.exports = app
